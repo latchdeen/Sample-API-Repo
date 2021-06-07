850d6d4beb4e4cce08bea3f076c730cd71437d3b
Latchman Deen <latchman@Latchmans-MacBook-Air.local>
2021-06-07 14:51:34 -0400

    commit

---
+++ SampleAPI/Controllers/ValuesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleAPI.Controllers
+{
+    [Route("marketdata")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        readonly List<string> Cities = new List<string> { "Hyderabad", "Bangalore" };
+        [HttpGet("v1/list")]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return Cities.ToArray();
+        }
+
+        [HttpGet("v1/data")]
+        public ActionResult<string> Get(int id)
+        {
+             string request = Request.Headers["Authorization"];
+           if (request !=" ")
+           {
+                return request;
+            }
+           else {
+               return "Requires authorization header";
+           }
+        }
+    }
+}
